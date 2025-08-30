1b6804125e63e95d0455ce794247921afdaa749e
Oliver Mills <oliver.mills@omnifederal.com>
2025-08-30 14:15:35 -0400

    api updates

--- app/routers/ai_router.py
+++ app/routers/ai_router.py
@@ -8,9 +8,16 @@ router = APIRouter(prefix="/would-you-rather")
 
 @router.get("/train", response_class=HTMLResponse)
 def train_model(model: WyrModelTrainer = Depends(get_model)):
-    model.train()
-    return Response(status_code=200)
+    try:
+        model.train()
+        return Response(content="Model successfully trained", status_code=200)
+    except Exception as e:
+        return Response(content=f"Model not successfully trained: {e}",status_code=500)
 
 @router.get("/{prompt}", response_class=Response)
 def generate_text(prompt: str, model: WyrModelTrainer = Depends(get_model)):
-    return Response(content=model.generate_text(prompt), media_type="text/plain")
+    try:
+        generated_text = model.generate_text(prompt)
+        return Response(content=generated_text, media_type="text/plain", status_code=200)
+    except Exception as e:
+        return Response(content=str(e), status_code=500)
